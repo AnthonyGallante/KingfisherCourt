6a2abe257e273b315325ad5a18be2cfe85d23ff5
Anthony Gallante <113798653+AnthonyGallante@users.noreply.github.com>
2025-12-23 20:00:29 -0700

    Moved simulate_n_games() to the simulations.py file. Moved "Team" column to front of dataframe

--- simulations.py
+++ simulations.py
@@ -4,7 +4,8 @@ from constants import TEAMS_DATA, STAT_EFFECTS, POSSESSIONS_STD_DEV
 import pickle
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
+from collections import Counter
+from tqdm import tqdm, trange
 
 with open(TEAMS_DATA, 'rb') as f:
     teams = pickle.load(f)
@@ -104,3 +105,93 @@ def simulate_game(team_1: Team, team_2: Team):
 
     output['Game Log'] = game_log
     return output
+
+
+def parse_game_log(log):
+    
+    team_1_log = log[0]
+    team_2_log = log[1]
+
+    team_1_players = list(team_1_log.keys())
+    team_2_players = list(team_2_log.keys())
+    players = [team_1_players, team_2_players]
+
+    event_log = {}
+    for player in (team_1_players + team_2_players):
+        event_log[player] = {}
+
+    for i, team_log in enumerate([team_1_log, team_2_log]):
+        for player in players[i]:
+            for event, count  in team_log[player][2].items():
+                if not event_log.get(player).get(event):
+                    event_log[player][event] = count
+                else:
+                    event_log[player][event] += count
+
+    return event_log
+
+
+def simulate_n_games(team_1, team_2, n: int, summary=False, viz=False):
+    print(f'Simulating Game: {team_1.name} vs. {team_2.name}')
+
+    team_1_win_counter = 0
+    team_2_win_counter = 0
+    game_results = {
+        team_1.name: [],
+        team_2.name: []
+    }
+    
+    total_stats_log = {}
+    player_games_count = Counter() # Specifically for "Games Played"
+
+    for i in trange(n):
+        game = simulate_game(team_1, team_2)
+        
+        # 2. Append Scores for statistical analysis
+        # We match the score to the correct team name list
+        if game['Winner'] == team_1.name:
+            team_1_win_counter += 1
+            game_results[team_1.name].append(game['Winner Score'])
+            game_results[team_2.name].append(game['Loser Score'])
+        else:
+            team_2_win_counter += 1
+            game_results[team_2.name].append(game['Winner Score'])
+            game_results[team_1.name].append(game['Loser Score'])
+
+        # 3. Parse and Update Stats
+        game_log = parse_game_log(game['Game Log'])
+
+        for player, stats in game_log.items():
+            # Track participation
+            player_games_count[player] += 1
+            
+            # Update cumulative stats
+            if player not in total_stats_log:
+                total_stats_log[player] = Counter(stats)
+            else:
+                total_stats_log[player].update(stats)
+
+    # Final Processing
+    team_1_players = list(team_1.df.index)
+
+    df = pd.DataFrame(total_stats_log).T.fillna(0)
+    df['Games Played'] = df.index.map(player_games_count)
+    df.insert(loc=0, column='Team', value=[team_1.name if p in team_1_players else team_2.name for p in list(df.index)])
+
+    # Converting numbers in the dataframe to integers
+    numeric_cols = df.select_dtypes(include=np.number).columns
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col]).astype(int)
+
+    overall_winner = team_1.name if team_1_win_counter > team_2_win_counter else team_2.name
+    overall_loser = team_1.name if team_1_win_counter < team_2_win_counter else team_2.name
+    record = f'{team_1.name}: {team_1_win_counter} - {team_2_win_counter} :{team_2.name}'
+
+    print(f'{team_1.name} vs. {team_2.name} Complete!')
+
+    return {'Winner': overall_winner,
+             'Loser': overall_loser, 
+             'Contributions': df, 
+             'Scores': game_results, 
+             'Record': record
+            }
